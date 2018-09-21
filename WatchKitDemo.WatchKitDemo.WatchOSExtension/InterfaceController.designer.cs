199e61af8de6b7f08c3553b2756c46de33047c41
Land Lu <v-luting@microsoft.com>
2018-09-21 18:18:07 +0800

    Add project files.

---
+++ WatchKitDemo.WatchKitDemo.WatchOSExtension/InterfaceController.designer.cs
@@ -0,0 +1,32 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace WatchKitDemo.WatchKitDemo.WatchOSExtension
+{
+    [Register ("InterfaceController")]
+    partial class InterfaceController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        WatchKit.WKInterfaceLabel MyLabel { get; set; }
+
+        [Action ("BtnClick")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnClick ();
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (MyLabel != null) {
+                MyLabel.Dispose ();
+                MyLabel = null;
+            }
+        }
+    }
+}
